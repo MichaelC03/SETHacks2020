5d3d4e0e28d923a679161aca8b0d4530146f0d2c
alexl0806 <52643676+alexl0806@users.noreply.github.com>
2020-07-18 12:28:41 -0400

    added variables

--- Main/src/BackEnd.java
+++ Main/src/BackEnd.java
@@ -10,4 +10,26 @@ Description: The backend (variable change, synthesizing data, etc) portion of th
  */
 public class BackEnd {
 
+  //variables for speed
+  static int speed = 1;
+
+  //variables for fish
+  static int numFish = 0;
+  static int weight = 0;
+  static int lifespan = 10;
+  static int food = 10;
+  static int fishOutput = 5;
+
+  //variables for plants
+  static int numPlants = 0;
+  static int lifeSpan = 0;
+  static int plantInput = 0;
+  static int plantOutput = 0;
+  static int lifespan = 5;
+
+  //variables for the tank
+  static int pH = 7;
+  static int
+
+
 }
