cf1f20662f98a5a9b761d3d3dfe028c6067b9dd9
alexl0806 <52643676+alexl0806@users.noreply.github.com>
2020-07-18 11:30:57 -0400

    updated description

--- Main/src/BackEnd.java
+++ Main/src/BackEnd.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Author: Alex Liu, Michael Cheng
+Date: July 18th, 2020
+Description: The backend (variable change, synthesizing data, etc) portion of the project
  */
 
 /**
@@ -9,5 +9,5 @@
  * @author Admin
  */
 public class BackEnd {
-    
+
 }
