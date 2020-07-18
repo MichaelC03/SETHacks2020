e2a74eee3f563f8473c6eed7eb7951b3b6f166f3
Your Name <mchen11@ocdsb.ca>
2020-07-18 12:39:34 -0400

    Added variables and timer

--- Main/src/BackEnd.java
+++ Main/src/BackEnd.java
@@ -1,13 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.Timer;
+import java.util.TimerTask;
 
-/**
- *
- * @author Admin
- */
-public class BackEnd {
+public class BackEnd
+{
+    //Variables
+    private static int speed = 2;
+    private int numPlants = 1;
+    private int numFish = 1;
     
+    //Timer
+    private static Timer myTimer;
+    private static long totalElapsedTime = 0;
+    private static long elapsedTime = 0;
+    
+    //Change the speed of the simulation
+    public void changeSpd(int s)
+    {
+        speed = s;
+    }
+    
+    //Change the number of plants
+    public void changePlants(int n)
+    {
+        numPlants = n;
+    }
+    
+    //Change the number of fish
+    public void changeFish(int n)
+    {
+        numFish = n;
+    }
+    
+    //Start the timer
+    public static void startTime() {
+        long startTime = System.currentTimeMillis();
+        
+        myTimer=new Timer();
+        
+        TimerTask task=new TimerTask() {
+            @Override
+            public void run() {
+                elapsedTime = (System.currentTimeMillis() - startTime) * speed + totalElapsedTime;
+                long elapsedSeconds = elapsedTime / 1000;
+                long secondsDisplay = elapsedSeconds % 60;
+                long elapsedMinutes = elapsedSeconds / 60;
+                System.out.println(String.format("%02d:%02d", elapsedMinutes, secondsDisplay));
+               }
+        };
+
+        myTimer.schedule(task,0,1000/speed);
+    }
+    
+    //Pause the timer
+    public static void pause()
+    {
+        totalElapsedTime = elapsedTime;
+        myTimer.cancel();
+    }
+    
+    //Resume the timer
+    public static void resume()
+    {
+        startTime();
+    }
+    
+    public static void main(String[] args)
+    {
+        startTime();
+    }
 }
