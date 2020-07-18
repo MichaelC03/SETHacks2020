1df66c652b40dbe03c9978db127985282f01c9bc
alexl0806 <52643676+alexl0806@users.noreply.github.com>
2020-07-18 14:04:57 -0400

    added plant and some variables

---
+++ Main/src/Plant.java
@@ -0,0 +1,22 @@
+/*
+Author: Alex Liu, Michael Cheng
+Date: July 18th, 2020
+Description: Plant Objects
+*/
+
+public class Plant {
+  private int numPlants;
+  private int plantHealth;
+  private double plantInput;
+  private int plantOutput;
+  private int plantLifespan = 130;
+
+  public Plant() {
+    numPlants = 1;
+    plantHealth = 4;
+    plantInput = 1.5;
+    plantOutput = 1.5;
+  }
+
+
+}
