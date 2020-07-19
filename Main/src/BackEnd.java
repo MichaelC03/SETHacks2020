8ec31365578f74aa6fa39222b3daf68224a19af3
Your Name <mchen11@ocdsb.ca>
2020-07-18 21:22:46 -0400

    Random methods

--- Main/src/BackEnd.java
+++ Main/src/BackEnd.java
@@ -6,7 +6,7 @@ import java.util.TimerTask;
 Author: Alex Liu, Michael Cheng
 Date: July 18th, 2020
 Description: The backend (variable change, synthesizing data, etc) portion of the project
- */
+*/
 
 public class BackEnd {
 
